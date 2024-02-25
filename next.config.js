76baa9f9e010cb65e7fa08219b1f9684adb924f2
Adam Davies <acdavies001@gmail.com>
2024-02-25 16:46:06 -0600

    Configure ports

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const PROXY_PORT = process.env.PROXY_PORT;
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/spotify-playlist-sync/:path*',
+        destination: `http://localhost:${PROXY_PORT}/spotify-playlist-sync/:path*`,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/spotify-playlist-sync',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: `http://localhost:${PROXY_PORT}`,
+          },
+        ],
+      },
+    ];
+  },
+};
