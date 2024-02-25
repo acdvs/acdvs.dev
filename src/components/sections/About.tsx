09c7ec79ce340fe95cf6ffe2ab45732b99821709
Adam Davies <acdavies001@gmail.com>
2024-02-25 17:11:46 -0600

    Update about

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,11 +4,18 @@ function About() {
   return (
     <Section title="About">
       <p>
-        I'm a full stack software engineer with a passion for building pixel-perfect fully
-        accessible UIs, powerful APIs, and performant microservices. In past roles, I've
-        led the planning, design, and execution of numerous features both big and small. I
-        exceed in technical writing, documentation, and communication as well as clean
-        code and codebase standardization.
+        I&apos;m a full stack software engineer with a passion for building pixel-perfect
+        fully accessible UIs, powerful APIs, and performant microservices. I&apos;ve led
+        the planning, design, and execution of numerous impactful features in
+        client-facing roles, improving efficiency by up to five times and greatly
+        simplifying complex processes.
+      </p>
+      <p>
+        I excel in rapidly developing SPAs and MPAs with React, TypeScript, and Next.js,
+        utilizing SSR where possible for minimal TTFB and client bandwidth. I have
+        experience with a variety of back end technologies -- namely Node.js for
+        single-language projects, Golang (Go) for fast APIs, and Rust for
+        computation-intensive WebAssembly solutions.
       </p>
     </Section>
   );
