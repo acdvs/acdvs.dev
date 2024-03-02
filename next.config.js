89dc1d16d8f238b86f08d1bc876eb5b388058994
Adam Davies <acdavies001@gmail.com>
2024-03-01 18:27:41 -0600

    Use static export

--- next.config.js
+++ next.config.js
@@ -2,25 +2,5 @@ const PROXY_PORT = process.env.PROXY_PORT;
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/spotify-playlist-sync/:path*',
-        destination: `http://localhost:${PROXY_PORT}/spotify-playlist-sync/:path*`,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/spotify-playlist-sync',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: `http://localhost:${PROXY_PORT}`,
-          },
-        ],
-      },
-    ];
-  },
+  output: 'export',
 };
