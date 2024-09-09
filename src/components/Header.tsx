b8785754eec704e416988607c9025803e4509091
Adam Davies <acdavies001@gmail.com>
2024-09-09 15:29:42 -0500

    Add link alt text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,17 @@ const Header = () => (
       <Logo className="w-8 h-8" />
     </a>
     <div className="flex gap-4">
-      <a href="https://linkedin.com/in/adamdavies001" target="_blank">
+      <a href="https://linkedin.com/in/adamdavies001" target="_blank" title="LinkedIn">
         <RiLinkedinFill className="w-8 h-8" />
       </a>
-      <a href="https://github.com/acdvs" target="_blank">
+      <a href="https://github.com/acdvs" target="_blank" title="GitHub">
         <RiGithubFill className="w-8 h-8" />
       </a>
-      <a href="https://stackoverflow.com/users/4881192/adam" target="_blank">
+      <a
+        href="https://stackoverflow.com/users/4881192/adam"
+        target="_blank"
+        title="Stack Overflow"
+      >
         <RiStackOverflowFill className="w-8 h-8" />
       </a>
     </div>
