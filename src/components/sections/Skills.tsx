99982db576ac1dfd9019f773c58f1878df88ffe8
Adam Davies <acdavies001@gmail.com>
2024-09-09 18:06:23 -0500

    Restructure skills

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,32 +1,29 @@
 import Section from '../Section';
+import { skills } from '@/data';
 
 function Skills() {
   return (
     <Section title="Skills">
-      <div className="mb-7">
-        <h2>Front End</h2>
-        <p>
-          TypeScript, JavaScript, React, Next.js, Tailwind, SCSS, CSS-in-JS, React Query,
-          Zustand, Redux, WebAssembly, WebGL, UI/UX design, accessibility, Figma
-        </p>
-      </div>
-      <div className="mb-7">
-        <h2>Back End</h2>
-        <p>
-          Node.js, Rust, Go, Python, GraphQL, MongoDB, PostgreSQL, API design,
-          microservices, AWS (S3, EC2, ECS, Lambda, API Gateway, Amplify, VPC),
-          DigitalOcean, CI/CD, Git, GitHub
-        </p>
-      </div>
-      <div className="mb-7">
-        <h2>Software</h2>
-        <p>
-          Photoshop, Illustrator, After Effects, Premiere Pro, InDesign, TouchDesigner,
-          Cinema 4D
-        </p>
-      </div>
+      {skills.map((x) => (
+        <Group key={x.label} label={x.label} list={x.list} />
+      ))}
     </Section>
   );
 }
+
+const Group = ({ label, list }: { label: string; list: string[] }) => (
+  <div className="flex flex-col sm:flex-row sm:items-center gap-5 mb-6">
+    <h3>{label}</h3>
+    <div className="flex flex-1 flex-wrap gap-2">
+      {list.map((x) => (
+        <Skill key={x} text={x} />
+      ))}
+    </div>
+  </div>
+);
+
+const Skill = ({ text }: { text: string }) => (
+  <span className="border-2 border-zinc-100/40 rounded-md px-1">{text}</span>
+);
 
 export default Skills;
