4d3a976116aa26ae5a676e02b728d6ae7d2f4795
Adam Davies <acdavies001@gmail.com>
2024-09-09 14:05:56 -0500

    Simplify Tailwind content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{ts,tsx,mdx}', './src/components/**/*.{ts,tsx,mdx}'],
+  content: ['./src/**/*.{ts,tsx,mdx}'],
   theme: {
     colors: {
       darkBlue: '#030b16',
