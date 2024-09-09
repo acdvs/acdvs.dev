6a591d86e807f47180e7da5e433111f1bdeb7fbf
Adam Davies <acdavies001@gmail.com>
2024-09-09 15:26:42 -0500

    Simplify intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,13 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="pb-[100px]">
-        <div className="h-screen flex justify-center items-center mb-5">
-          <div className="max-w-xl px-10 position-absolute top-50 translate-middle-y">
-            <div>
-              <h1 className="text-center">
-                Hi, I&apos;m <span className="text-gradient m-0">Adam</span>.
-              </h1>
-              <p className="mb-10 text-center">
-                Full stack software engineer and open source contributor with expertise in
-                UI and UX design.
-              </p>
-            </div>
-          </div>
+      <main>
+        <div className="mb-10">
+          <h1>Adam Davies</h1>
+          <p>
+            Full stack software engineer and open source contributor with expertise in UI
+            and UX design.
+          </p>
         </div>
         <Sections.About />
         <Sections.Skills />
