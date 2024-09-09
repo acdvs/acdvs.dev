4aa8b2d4580d59f13224ed9fbbbbf06cf767a99c
Adam Davies <acdavies001@gmail.com>
2024-09-09 14:44:42 -0500

    Remove smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body className="bg-gradient">{children}</body>
       <GoogleAnalytics gaId="G-1VFVVMH9QR" />
     </html>
