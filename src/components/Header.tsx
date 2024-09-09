e436fc610c985625d701e198b2bbf3620fd6b11a
Adam Davies <acdavies001@gmail.com>
2024-09-09 15:25:55 -0500

    Collapse syntax

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,23 @@
 import { RiGithubFill, RiLinkedinFill, RiStackOverflowFill } from '@remixicon/react';
 import Logo from './Logo';
 
-function Header() {
-  return (
-    <header className="w-full sticky top-0 flex justify-between md:p-5 header-gradient">
-      <a href="#">
-        <Logo className="w-10 h-10" />
+const Header = () => (
+  <header className="w-full sticky top-0 mb-10 flex justify-between header-gradient">
+    <a href="#">
+      <Logo className="w-8 h-8" />
+    </a>
+    <div className="flex gap-4">
+      <a href="https://linkedin.com/in/adamdavies001" target="_blank">
+        <RiLinkedinFill className="w-8 h-8" />
       </a>
-      <div className="flex gap-4">
-        <a href="https://linkedin.com/in/adamdavies001" target="_blank">
-          <RiLinkedinFill className="w-10 h-10" />
-        </a>
-        <a href="https://github.com/acdvs" target="_blank">
-          <RiGithubFill className="w-10 h-10" />
-        </a>
-        <a href="https://stackoverflow.com/users/4881192/adam" target="_blank">
-          <RiStackOverflowFill className="w-10 h-10" />
-        </a>
-      </div>
-    </header>
-  );
-}
+      <a href="https://github.com/acdvs" target="_blank">
+        <RiGithubFill className="w-8 h-8" />
+      </a>
+      <a href="https://stackoverflow.com/users/4881192/adam" target="_blank">
+        <RiStackOverflowFill className="w-8 h-8" />
+      </a>
+    </div>
+  </header>
+);
 
 export default Header;
