3b1e179d5f74c1576bea416388ac87122960b269
Adam Davies <acdavies001@gmail.com>
2024-09-09 14:42:31 -0500

    Move button styles to base a

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,26 +4,18 @@ import Logo from './Logo';
 function Header() {
   return (
     <header className="w-full sticky top-0 flex justify-between md:p-5 header-gradient">
-      <a href="#" className="btn-primary">
-        <Logo className="w-10" />
+      <a href="#">
+        <Logo className="w-10 h-10" />
       </a>
       <div className="flex">
-        <a
-          href="https://linkedin.com/in/adamdavies001"
-          target="_blank"
-          className="btn-primary"
-        >
-          <RiLinkedinFill className="w-10" />
+        <a href="https://linkedin.com/in/adamdavies001" target="_blank">
+          <RiLinkedinFill className="w-10 h-10" />
         </a>
-        <a href="https://github.com/acdvs" target="_blank" className="btn-primary">
-          <RiGithubFill className="w-10" />
+        <a href="https://github.com/acdvs" target="_blank">
+          <RiGithubFill className="w-10 h-10" />
         </a>
-        <a
-          href="https://stackoverflow.com/users/4881192/adam"
-          target="_blank"
-          className="btn-primary"
-        >
-          <RiStackOverflowFill className="w-10" />
+        <a href="https://stackoverflow.com/users/4881192/adam" target="_blank">
+          <RiStackOverflowFill className="w-10 h-10" />
         </a>
       </div>
     </header>
