02ad96497aaebb141789f9825c505c4840cf577f
Adam Davies <acdavies001@gmail.com>
2024-09-09 16:59:02 -0500

    Reorganize intro with grid

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -1,42 +1,42 @@
 import { RiGithubFill, RiLinkedinFill, RiStackOverflowFill } from '@remixicon/react';
+import Section from '../Section';
 
 const IntroSection = () => (
-  <div className="flex justify-between gap-14 mb-10">
-    <div className="flex-1">
-      <h1>Adam Davies</h1>
-      <p>
-        Front end engineer and open source contributor with expertise in UI and UX design.
+  <Section>
+    <div className="grid grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-14 gap-y-4">
+      <h1 className="mb-0">Adam Davies</h1>
+      <p className="order-last col-span-2 sm:col-span-1">
+        Front end engineer with a passion for pixel-perfect design and accessibility
+        standards. Bringing valuable UI/UX insight and direction to any project.
       </p>
-    </div>
-    <div>
-      <div className="flex flex-col gap-2">
+      <div className="row-span-1 sm:row-span-2 flex sm:flex-col gap-4 sm:gap-2 sm:self-start">
         <a
           href="https://linkedin.com/in/adamdavies001"
           target="_blank"
           className="flex gap-3 items-center"
         >
-          <RiLinkedinFill className="w-6 h-6" />
-          <p className="mb-0">LinkedIn</p>
+          <RiLinkedinFill className="w-7 h-7" />
+          <p className="mb-0 hidden sm:block">LinkedIn</p>
         </a>
         <a
           href="https://github.com/acdvs"
           target="_blank"
           className="flex gap-3 items-center"
         >
-          <RiGithubFill className="w-6 h-6" />
-          <p className="mb-0">GitHub</p>
+          <RiGithubFill className="w-7 h-7" />
+          <p className="mb-0 hidden sm:block">GitHub</p>
         </a>
         <a
           href="https://stackoverflow.com/users/4881192/adam"
           target="_blank"
           className="flex gap-3 items-center"
         >
-          <RiStackOverflowFill className="w-6 h-6" />
-          <p className="mb-0">Stack Overflow</p>
+          <RiStackOverflowFill className="w-7 h-7" />
+          <p className="mb-0 hidden sm:block">Stack Overflow</p>
         </a>
       </div>
     </div>
-  </div>
+  </Section>
 );
 
 export default IntroSection;
