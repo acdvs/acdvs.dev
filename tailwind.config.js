d5824166f963825d27626ba4c40c587f446ab7b3
Adam Davies <acdavies001@gmail.com>
2024-09-09 14:43:29 -0500

    Remove custom Tailwind values

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{ts,tsx,mdx}'],
   theme: {
-    colors: {
-      darkBlue: '#030b16',
-      mediumBlue: '#0a58ca',
-      blue: '#354ffe',
-      lightBlue: '#33b4f7',
-      black: '#000000',
-      white: '#ffffff',
-      transparent: 'transparent',
-      darkTrans1: '#ffffff77',
-      darkTrans2: '#ffffff11',
-    },
     fontFamily: {
       montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
       body: ['Open Sans', ...defaultTheme.fontFamily.sans],
     },
     extend: {
-      borderWidth: {
-        1: '1px',
-      },
       screens: {
         xs: '450px',
       },
