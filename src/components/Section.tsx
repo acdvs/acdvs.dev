ddddfe03cce0b5cdf83f68e2580524321597d590
Adam Davies <acdavies001@gmail.com>
2024-09-09 15:51:37 -0500

    Make Section title optional

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,11 +1,11 @@
 type Props = React.PropsWithChildren & {
-  title: string;
+  title?: string;
 };
 
 const Section = ({ title, children }: Props) => {
   return (
-    <div className="mb-24">
-      <h2 className="text-gradient">{title}</h2>
+    <div className="mb-16">
+      {title && <h2 className="text-gradient">{title}</h2>}
       {children}
     </div>
   );
