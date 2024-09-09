0463bd9c46938f95cd35b93e73890888b9f0be68
Adam Davies <acdavies001@gmail.com>
2024-09-09 15:26:10 -0500

    Simplify Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,14 +2,13 @@ type Props = React.PropsWithChildren & {
   title: string;
 };
 
-function Section({ title, children }: Props) {
+const Section = ({ title, children }: Props) => {
   return (
-    <div className="max-w-[750px] px-4 mx-auto mb-24">
-      <div id={title.toLowerCase()} className="relative top-[-11vw]" />
-      <h1 className="text-gradient mb-4">{title}</h1>
+    <div className="mb-24">
+      <h2 className="text-gradient">{title}</h2>
       {children}
     </div>
   );
-}
+};
 
 export default Section;
