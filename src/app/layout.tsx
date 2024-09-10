92ead31603a2344264a0e8038512cbf0359d78ca
Adam Davies <acdavies001@gmail.com>
2024-09-09 19:16:57 -0500

    Fix background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gradient">{children}</body>
+      <body>{children}</body>
       <GoogleAnalytics gaId="G-1VFVVMH9QR" />
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import Sections from '@/components/sections';
 
 export default function Index() {
   return (
-    <div className="max-w-[700px] mx-auto p-5 sm:pt-10">
-      <Sections.Intro />
-      <Sections.About />
-      <Sections.Skills />
-      <Sections.Projects />
-    </div>
+    <>
+      <div className="max-w-[700px] mx-auto p-5 sm:pt-10">
+        <Sections.Intro />
+        <Sections.About />
+        <Sections.Skills />
+        <Sections.Projects />
+      </div>
+      <div className="bg-gradient fixed inset-0 -z-10" />
+    </>
   );
 }
