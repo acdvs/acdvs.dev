3b73c853afaccb38e0c41ff59ddf1073b7307acf
Adam Davies <acdavies001@gmail.com>
2025-04-26 20:35:46 -0500

    Update intro

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -7,8 +7,9 @@ function IntroSection() {
       <div className="grid grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-14 gap-y-4">
         <h1 className="mb-0">Adam Davies</h1>
         <p className="order-last col-span-2 sm:col-span-1">
-          Front end engineer with a passion for pixel-perfect design and accessibility
-          standards. Bringing valuable UI/UX insight and direction to any project.
+          Full stack engineer building websites and tools that look good, work well, and
+          make a difference. Frequent open source contributor and real-time audiovisual
+          hobbyist.
         </p>
         <div className="row-span-1 sm:row-span-2 flex sm:flex-col gap-4 sm:gap-2 sm:self-start">
           <a
