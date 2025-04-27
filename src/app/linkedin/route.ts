4f16e5f1603bc4e0880e9b779251e3ef9fa5d1a1
Adam Davies <acdavies001@gmail.com>
2025-04-26 21:34:33 -0500

    Add social media redirects

---
+++ src/app/linkedin/route.ts
@@ -0,0 +1,3 @@
+export function GET() {
+  return Response.redirect('https://linkedin.com/in/adamdavies001');
+}
---
+++ src/app/stackoverflow/route.ts
@@ -0,0 +1,3 @@
+export function GET() {
+  return Response.redirect('https://stackoverflow.com/users/4881192/adam');
+}
