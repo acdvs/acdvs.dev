86336420117bc78f2c36b89f5adbf2a7dde53ca5
Adam Davies <acdavies001@gmail.com>
2025-04-26 22:33:49 -0500

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-1/2 text-center">
+      <p>Nothing to see here.</p>
+      <a href="/" className="font-bold uppercase">
+        Go back
+      </a>
+    </div>
+  );
+}
