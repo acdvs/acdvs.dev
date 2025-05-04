956cb21964b36ba8b71946b1c806c0d6277958b0
Adam Davies <acdavies001@gmail.com>
2025-05-03 19:12:24 -0500

    Change tag style

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,6 @@
 function Tag({ label }: { label: string }) {
   return (
-    <span className="text-sm border-2 border-zinc-100/40 rounded-md px-1 cursor-default">
+    <span className="text-sm border-2 border-slate-600 rounded-md px-1 cursor-default">
       {label}
     </span>
   );
