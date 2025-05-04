738e7c077746eeba707cbf97cc7c3d5c872f058c
Adam Davies <acdavies001@gmail.com>
2025-05-03 19:12:13 -0500

    Simplify accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,64 +1,42 @@
-// Adapted from
-// https://ui.shadcn.com/docs/components/accordion
-
 'use client';
 
-import * as React from 'react';
-import * as AccordionPrimitive from '@radix-ui/react-accordion';
+import { useState } from 'react';
 import { ChevronDown } from 'lucide-react';
 import clsx from 'clsx';
 
-const Accordion = AccordionPrimitive.Root;
-
 function AccordionItem({
-  className,
-  ...props
-}: React.ComponentProps<typeof AccordionPrimitive.Item>) {
-  return (
-    <AccordionPrimitive.Item
-      data-slot="accordion-item"
-      className={clsx('border-b', className)}
-      {...props}
-    />
-  );
-}
-
-function AccordionTrigger({
+  label,
   className,
   children,
-  ...props
-}: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+}: {
+  label: string;
+  className?: string;
+  children: React.ReactNode;
+}) {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <AccordionPrimitive.Header className="flex">
-      <AccordionPrimitive.Trigger
-        data-slot="accordion-trigger"
+    <div className="border-b-2 border-slate-600 hover:border-slate-400">
+      <div
+        className="py-2 flex justify-between cursor-pointer"
+        onClick={() => setOpen((x) => !x)}
+      >
+        <p className="m-0 font-bold">{label}</p>
+        <ChevronDown
+          className={clsx(isOpen && 'rotate-180', 'w-5 transition-transform')}
+        />
+      </div>
+      <div
         className={clsx(
-          'flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180',
           className,
+          isOpen ? 'auto-height mb-3' : 'h-0',
+          'overflow-hidden transition-all',
         )}
-        {...props}
       >
         {children}
-        <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
-      </AccordionPrimitive.Trigger>
-    </AccordionPrimitive.Header>
+      </div>
+    </div>
   );
 }
 
-function AccordionContent({
-  className,
-  children,
-  ...props
-}: React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>) {
-  return (
-    <AccordionPrimitive.Content
-      data-slot="accordion-content"
-      className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
-      {...props}
-    >
-      <div className={clsx('pb-4 pt-0', className)}>{children}</div>
-    </AccordionPrimitive.Content>
-  );
-}
-
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+export { AccordionItem };
--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,9 +1,4 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '../Accordion';
+import { AccordionItem } from '../Accordion';
 import Section from '../Section';
 import Tag from '../Tag';
 
@@ -13,19 +8,15 @@ function Skills() {
   return (
     <Section title="Skills">
       {skills.map((x) => (
-        <Accordion type="multiple" key={x.label}>
-          <AccordionItem
-            value={x.label}
-            className="border-b-2 border-b-zinc-100/40 hover:border-b-zinc-100/70 transition-colors"
-          >
-            <AccordionTrigger>{x.label}</AccordionTrigger>
-            <AccordionContent className="flex flex-1 flex-wrap gap-2">
-              {x.list.map((x) => (
-                <Tag key={x} label={x} />
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+        <AccordionItem
+          key={x.label}
+          label={x.label}
+          className="flex flex-1 flex-wrap gap-2"
+        >
+          {x.list.map((x) => (
+            <Tag key={x} label={x} />
+          ))}
+        </AccordionItem>
       ))}
     </Section>
   );
