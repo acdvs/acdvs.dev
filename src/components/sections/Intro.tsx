4d063c7ad953e4321dba8f7a42df92da3690de36
Adam Davies <acdavies001@gmail.com>
2025-05-05 01:46:03 -0500

    Improve top links a11y

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -4,39 +4,39 @@ import Section from '../Section';
 function IntroSection() {
   return (
     <Section className="grid grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-14 gap-y-4">
-        <h1 className="mb-0">Adam Davies</h1>
-        <p className="order-last col-span-2 sm:col-span-1">
-          Full stack engineer building websites and tools that look good, work well, and
-          make a difference. Frequent open source contributor and real-time audiovisual
-          hobbyist.
-        </p>
-        <aside className="row-span-1 sm:row-span-2 flex sm:flex-col gap-4 sm:gap-2 sm:self-start">
-          <a
-            href="https://linkedin.com/in/adamdavies001"
-            target="_blank"
-            className="flex gap-3 items-center"
-          >
-            <RiLinkedinFill className="w-7 h-7" />
-            <p className="mb-0 hidden sm:block">LinkedIn</p>
-          </a>
-          <a
-            href="https://github.com/acdvs"
-            target="_blank"
-            className="flex gap-3 items-center"
-          >
-            <RiGithubFill className="w-7 h-7" />
-            <p className="mb-0 hidden sm:block">GitHub</p>
-          </a>
-          <a
-            href="https://stackoverflow.com/users/4881192/adam"
-            target="_blank"
-            className="flex gap-3 items-center"
-          >
-            <RiStackOverflowFill className="w-7 h-7" />
-            <p className="mb-0 hidden sm:block">Stack Overflow</p>
-          </a>
-        </aside>
-      </div>
+      <h1 className="mb-0">Adam Davies</h1>
+      <p className="order-last col-span-2 sm:col-span-1">
+        Full stack engineer building websites and tools that look good, work well, and
+        make a difference. Frequent open source contributor and real-time audiovisual
+        hobbyist.
+      </p>
+      <aside className="row-span-1 sm:row-span-2 flex sm:flex-col gap-4 sm:gap-2 sm:self-start">
+        <h2 className="sr-only">External Links</h2>
+        <a
+          href="https://linkedin.com/in/adamdavies001"
+          target="_blank"
+          className="flex gap-3 items-center"
+        >
+          <RiLinkedinFill className="size-7" />
+          <p className="mb-0 sr-only sm:not-sr-only">LinkedIn</p>
+        </a>
+        <a
+          href="https://github.com/acdvs"
+          target="_blank"
+          className="flex gap-3 items-center"
+        >
+          <RiGithubFill className="size-7" />
+          <p className="mb-0 sr-only sm:not-sr-only">GitHub</p>
+        </a>
+        <a
+          href="https://stackoverflow.com/users/4881192/adam"
+          target="_blank"
+          className="flex gap-3 items-center"
+        >
+          <RiStackOverflowFill className="size-7" />
+          <p className="mb-0 sr-only sm:not-sr-only">Stack Overflow</p>
+        </a>
+      </aside>
     </Section>
   );
 }
