b84aa9906c7652a767c5698b4abf673ae8e2ca96
Adam Davies <acdavies001@gmail.com>
2025-05-05 02:13:02 -0500

    Implement Accordion HOC

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,8 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { ChevronDown } from 'lucide-react';
 import clsx from 'clsx';
+
+type TContextValue = string | null;
+type TContext = {
+  group: string;
+  activeItem: TContextValue;
+  setActiveItem: (x: TContextValue) => void;
+};
+
+const defaultContext = {
+  group: '',
+  activeItem: null,
+  setActiveItem: () => {},
+};
+
+const AccordionContext = createContext<TContext>(defaultContext);
+
+function Accordion({ group, children }: { group: string; children: React.ReactNode }) {
+  const [activeItem, setActiveItem] = useState<TContextValue>(null);
+
+  return (
+    <AccordionContext value={{ group, activeItem, setActiveItem }}>
+      {children}
+    </AccordionContext>
+  );
+}
 
 function AccordionItem({
   label,
@@ -13,25 +38,41 @@ function AccordionItem({
   className?: string;
   children: React.ReactNode;
 }) {
-  const [isOpen, setOpen] = useState(false);
+  const { group, activeItem, setActiveItem } = useContext(AccordionContext);
+
+  const contentId = `${group}-${label}-content`.replaceAll(' ', '');
+  const isOpen = activeItem === label;
+
+  const onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.code === 'Space' || e.code === 'Enter') {
+      e.preventDefault();
+      setActiveItem(isOpen ? null : label);
+    }
+  };
 
   return (
     <div className="border-b-2 border-slate-600 hover:border-slate-400">
       <div
-        className="py-2 flex justify-between cursor-pointer"
-        onClick={() => setOpen((x) => !x)}
+        className="py-2 w-full flex justify-between cursor-pointer"
+        onClick={() => setActiveItem(isOpen ? null : label)}
       >
-        <p className="m-0 font-bold">{label}</p>
+        <h3>
+          <button onKeyDown={onKeyDown} aria-expanded={isOpen} aria-controls={contentId}>
+            {label}
+          </button>
+        </h3>
         <ChevronDown
           className={clsx(isOpen && 'rotate-180', 'w-5 transition-transform')}
         />
       </div>
       <div
+        id={contentId}
         className={clsx(
           className,
           isOpen ? 'auto-height mb-3' : 'h-0',
           'overflow-hidden transition-[height]',
         )}
+        aria-hidden={!isOpen}
       >
         {children}
       </div>
@@ -39,4 +80,4 @@ function AccordionItem({
   );
 }
 
-export { AccordionItem };
+export { Accordion, AccordionItem };
--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,4 +1,4 @@
-import { AccordionItem } from '../Accordion';
+import { Accordion, AccordionItem } from '../Accordion';
 import Section from '../Section';
 import Tag from '../Tag';
 
@@ -7,17 +7,19 @@ import skills from '@/data/skills';
 function Skills() {
   return (
     <Section title="Skills">
-      {skills.map((x) => (
-        <AccordionItem key={x.label} label={x.label}>
-          <ul className="flex flex-1 flex-wrap gap-2">
-            {x.list.map((x) => (
-              <li key={x}>
-                <Tag label={x} />
-              </li>
-            ))}
-          </ul>
-        </AccordionItem>
-      ))}
+      <Accordion group="skills">
+        {skills.map((x) => (
+          <AccordionItem key={x.label} label={x.label}>
+            <ul className="flex flex-1 flex-wrap gap-2">
+              {x.list.map((x) => (
+                <li key={x}>
+                  <Tag label={x} />
+                </li>
+              ))}
+            </ul>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </Section>
   );
 }
