c6dd1296909d9c154e1a89ab134823ed309f4fff
Adam Davies <adam@acdvs.dev>
2026-05-27 16:20:39 -0500

    feat: improve error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,12 @@
 'use client';
 
 export default function ErrorPage() {
-  return <h1>There&apos;s nothing here.</h1>;
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-1/2 text-center">
+      <p>Nothing to see here.</p>
+      <a href="/" className="font-bold uppercase">
+        Go back
+      </a>
+    </div>
+  );
 }
